c4043ba74a37c9046f0c56c19c679ed54a97304a
Mohidul Abedin <mabedin00@stuy.edu>
2019-11-13 22:40:48 -0500

    finished

--- stat.c
+++ stat.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include<time.h>
 #include <string.h>
+#include <time.h>
+#include <math.h>
 
+void readable_perm(mode_t mode, char * buffer);
 int main(){
     struct stat meta;
     stat("stat.c", &meta);
-    printf("%ld \n", meta.st_size);
-    printf("%o \n", meta.st_mode);
-    printf("%s \n", ctime(&meta.st_mtime));
+    printf("Size: %ld \n", meta.st_size);
+    printf("Mode: %o \n", meta.st_mode);
+    printf("Last Edit: %s \n", ctime(&meta.st_mtime));
+
+    double fileSize = meta.st_size;
+    double fileSizeGB = fileSize / 1000000000;
+    double fileSizeMB = fileSize / 1000000;
+    double fileSizeKB = fileSize / 1000;
+    printf("HUMAN READABLE FORMAT %f GB, %f MB, %f KB, %f B \n", fileSizeGB, fileSizeMB, fileSizeKB, fileSize);
+    char permission[1];
+    readable_perm(meta.st_mode, permission);
+    printf("Permissions: %s\n", permission);
+    return 0;
+}
+
+void readable_perm(mode_t mode, char * buffer){
+  char perm[9] = "rwxrwxrwx";
+  int i;
+    for (i = 0; i < 9; i++){
+      if(!(mode & (1 << (8-i)))){
+        perm[i] = '-';
+      }
+    }
+  strcat(buffer, perm);
 }
